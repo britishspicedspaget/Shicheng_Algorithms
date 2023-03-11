40c2589db395f324a93ebdd7825b4b3b3b254d15
shicheng <shicheng_zhang@outlook.com>
2023-03-11 09:50:45 -0800

    A simple program for timer purposes

---
+++ second.c
@@ -0,0 +1,36 @@
+/*11/3 2023
+#
+# This is a C program designed to be an accurate timer to the amount of seconds that you enter.
+# it does not have a minute or hour calibration, and the inputs and output are purely in seco-
+# nds. Please note that any non-numerical input will give an instant output of 0.00001, and a-
+# ny mixed number-letter input will result in the timer counting the seconds of the first sec-
+# tion of the numerical input ONLY and may return values that are faulty to the programmer's 
+# purposes.
+#  
+# Please no not copy paste this program into your own program and claim credit, that is plagi-
+# arism and if discovered may lead to serious penalties. Copy and paste of this program is on-
+# ly permmited with a direct reference to the program writer(see below).
+#
+# Copyright 2023, Shicheng.Z
+#*/
+#include <stdio.h>
+int main()
+{
+        int number;
+        printf("Enter the seconds you want to set a timer to:");
+        scanf("%d",&number);
+        float numbernumber = 1;
+        while (1)
+        {
+                if((numbernumber / 100000) >= number)
+                {
+                        printf("%f\n",numbernumber / 100000);
+                        break;
+                }
+                else
+                {
+                        printf("%f\n",numbernumber / 100000);
+                        numbernumber = numbernumber + 2.6;
+                }       
+        }
+}
