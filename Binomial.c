3db6fcb28bf9b1a32b719f4caf14ab03efdc47fc
britishspicedspaget <126411346+britishspicedspaget@users.noreply.github.com>
2023-07-17 19:39:23 -0600

    A program that solves binomial probability with both half and also irregular singular probabilities

--- Binomial.c
+++ Binomial.c
@@ -1,54 +1,91 @@
-/* This is a simple c program designed to be a calculater for binomial distribution 
+/* This is a C program designed to be a calculator for binomial distribution 
 * and probability calculations using a pascals triangle as the coefficient finder
 * and also solving the probability by employing the sum of all the possibilities 
-* as calculated. Please note that this is a premature version, a newer version 
-* that can solve different singular probability questions and graph the results 
-* will be uploaded soon.
+* as calculated. There is also a function capable of running probability calculations
+* whose singualr theoretical probability is not that of 50% or 0.5, which runs on the 
+* binomial formula conerning probability and is supplemented by a nCr and a factorial 
+* sub-function for these calculations.
+*
+* The update that will enable the program to graph the results will be coming shortly.
 *
 * Please do not copy and paste :(
 *
-* Shicheng.Z  July 15th, 2023.
+* Shicheng.Z  OG: July 15th, 2023. UPDATED July 17th, 2023
 */
 #include <stdio.h>
+#include <math.h>
+int factorial(int number);
+int nCr(int number, int number2);
+int factorial(int number)
+{
+	if(number == 0)
+        {
+                number = 1;
+                return number;
+        }
+        for(int h = number - 1;h > 0;h--)
+        {
+                number = number * h;
+        }
+        return number;
+}
+int nCr(int number, int number2)
+{
+	return factorial(number) / (factorial(number2) * factorial(number - number2));
+}
 int main()
 {
-        int og_total, choice, prob;
-        printf("Enter the total amount of choices:");
-        scanf("%d",&og_total);
-        printf("Enter the choice:");
-        scanf("%d",&choice);
-        printf("ENter the probability:");
-        scanf("%d",&prob);
-        int total = og_total + 1;
-        int attr[total][total];
-        for(int s = 0;s < total;s++)
-        {
-                for(int u = 0;u < total;u++)
-                {
-                        attr[s][u] = 0;
-                }
-        }
-        attr[0][0] = 1;
-        attr[1][0] = 1;
-        attr[1][1] = 1;
-        for(int x = 2;x < total;x++)
-        {
-                for(int a = 0;a < total;a++)
-                {
-                        if(a == 0)
-                        {
-                                attr[x][a] = 1;
-                        }
-                        else
-                        {
-                                attr[x][a] = attr[x - 1][a] + attr[x - 1][a - 1];
-                        }
-                }
-        }
-        int result = attr[total - 1][0];
-        for(int l = 1;l < total;l++)
-        {
-                result = attr[total - 1][l] + result;
-        }
-        printf("Getting a %d chosen results out of %d og tosses in 2 mutually exclusive outcomes is %d / %d or %f percent\n",choice,og_total,attr[total - 1][choice],result,((float)attr[total - 1][choice] / (float)result) * 100);
+	int og_total, choice;
+	float prob;
+	printf("Enter the total amount of choices:");
+	scanf("%d",&og_total);
+	printf("Enter the choice:");
+	scanf("%d",&choice);
+	printf("Enter the probability for the event in question:");
+	scanf("%f",&prob);
+	if((prob > 0) && (prob < 1))
+	{
+		if(prob == 0.5)
+		{
+			int total = og_total + 1;
+			int attr[total][total];
+			for(int s = 0;s < total;s++)
+			{
+				for(int u = 0;u < total;u++)
+				{
+					attr[s][u] = 0;
+				}
+			}
+			attr[0][0] = 1;
+			attr[1][0] = 1;
+			attr[1][1] = 1;
+			for(int x = 2;x < total;x++)
+			{
+				for(int a = 0;a < total;a++)
+				{
+					if(a == 0)
+					{
+						attr[x][a] = 1;
+					}
+					else
+					{
+						attr[x][a] = attr[x - 1][a] + attr[x - 1][a - 1];
+					}
+				}
+				printf("\n");
+			}
+			int result = attr[total - 1][0];
+			for(int l = 1;l < total;l++)
+			{
+				result = attr[total - 1][l] + result;
+			}
+		printf("Getting a %d chosen results out of %d og tosses with %f as the singular probability in 2 mutually exclusive outcomes is %d / %d or %lf percent\n",choice,og_total,prob,attr[total - 1][choice],result,((double)attr[total - 1][choice] / (double)result) * 100);}
+		else
+		{
+			double answer;
+			answer = nCr(og_total,choice) * pow(prob,(double)choice) * pow((double)1 - prob,(double)(og_total - choice));
+			printf("Probability is equal to: %lf\n",answer);
+		}
+	}
 }
+
