f23d10e9b76ad0c6716a9bed7ba79b6b0bff7e1d
britishspicedspaget <126411346+britishspicedspaget@users.noreply.github.com>
2023-10-07 10:45:49 -0600

    A program that can solve quadratics using both the quadratic formula and also the quadratic equation, stage 1 commit

--- quadratics.c
+++ quadratics.c
@@ -35,17 +35,19 @@ int main()
         printf("Note: This calculator's equation follows ax2 + bx + c = 0 or x = -b +- sqrt(b2 - 4ac) / 2a format.\n");
         if(decision == 1)
         {
+                // Quadratic formula
                 testx1 = (-1 * b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
                 testx2 = (-1 * b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
                 printf("Test results: y = 0.0, x1 = %lf, x2 = %lf\n",testx1,testx2);
         }
+        //defining the x range of the graph
         int x1, x2;
-        printf("Select the y range for your graphing:");
+        printf("Select the x range for your graphing:");
         scanf("%d %d",&x1,&x2);
         head:
         if((ABS(x2 - x1) + 1) != size)
         {
-                printf("\nPlease re-enter your size of the y range(entered %d, defined maximum %d):",ABS(x2 - x1) + 1, size);
+                printf("\nPlease re-enter your size of the x range(entered %d, defined maximum %d):",ABS(x2 - x1) + 1, size);
                 scanf("%d %d",&x1, &x2);
                 goto head;
         }
@@ -64,6 +66,7 @@ int main()
         }
         for(int i = 0;i < size;i++)
         {
+                //Usage of quadratic equation
                 attry[i] = (a * x * x) + (b * x) + c;
                 printf("X %lf    Y %lf\n",x, attry[i]);
                 x++;
